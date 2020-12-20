244ac07b5ee74a7cf613812b6b21a64dd0dd89b6
Jimena Arranz <jimena@MacBook-Air-de-Jimena.local>
2020-12-20 21:23:20 +0100

    used @apply in main-content and body

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,33 @@ module.exports = {
         primary: "#d3fbd8",
         primaryTransparent: "d3fbd8a6",
         primaryHover: "#8df59a",
+        textColor: "#0043b1",
         secondary: "#6c757d",
         bgSecondary: "#b0b0b0",
         grayForm: "#ced4da",
         grayButton: "#212529"
       },
+      borderWidth: {
+        "5": "5px"
+      },
       screens: {
-        xs: "425px"
+        xs: "425px",
+        "3xl": "1800px"
       },
       fontSize: {
         h1: "2.5rem"
       },
       lineHeight: {
-        h1: "1.2"
+        h1: "1.2",
+        "30px": "30px"
+      },
+      maxWidth: {
+        "80": "80%",
+        "100px": "100px",
+        "150px": "150px"
+      },
+      spacing: {
+        "1440": "1440px"
       }
     }
   },
