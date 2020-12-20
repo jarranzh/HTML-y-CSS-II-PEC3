2711f585cb19d7e039858071f764b045cbde6fa4
Jimena Arranz <jimena@MacBook-Air-de-Jimena.local>
2020-12-20 14:19:16 +0100

    extract form component to use @apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,29 @@ module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      cyan: colors.cyan,
-      black: colors.black,
-      white: colors.white,
-      primary: "#d3fbd8",
-      primaryTransparent: "d3fbd8a6",
-      primaryHover: "#8df59a",
-      secondary: "#6c757d"
-    },
-    screens: {
-      xs: "425px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px"
-    },
-    // fontSize: {
-    //   h1: "2.5rem"
-    // },
-    extend: {}
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        black: colors.black,
+        white: colors.white,
+        primary: "#d3fbd8",
+        primaryTransparent: "d3fbd8a6",
+        primaryHover: "#8df59a",
+        secondary: "#6c757d",
+        bgSecondary: "#b0b0b0",
+        grayForm: "#ced4da",
+        grayButton: "#212529"
+      },
+      screens: {
+        xs: "425px"
+      },
+      fontSize: {
+        h1: "2.5rem"
+      },
+      lineHeight: {
+        h1: "1.2"
+      }
+    }
   },
   variants: {
     extend: {}
